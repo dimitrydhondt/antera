5632a37580c20e9710bec376a476d43f957ff23e
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2016-06-20 17:58:11 +0000

    update

--- generate.sh
+++ generate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo Procesing view-nl.json
 mustache -p menu.mustache -p footer.mustache view-nl.json index.mustache >public/index.html
-mustache -p menu.mustache -p footer.mustache view-nl.json services.mustache >public/services.html
+mustache -p menu.mustache -p footer.mustache view-nl.json sherborne.mustache >public/sherborne.html
+mustache -p menu.mustache -p footer.mustache view-nl.json peutertijd.mustache >public/peutertijd.html
 mustache -p menu.mustache -p footer.mustache view-nl.json contact.mustache >public/contact.html
