e8cf7611c8f7fc1cb7fa3f23891cdf2889dbdebc
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2016-09-21 20:15:03 +0000

    updates

--- generate.sh
+++ generate.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo Procesing view-nl.json
 mustache -p menu.mustache -p footer.mustache view-nl.json index.mustache >public/index.html
-mustache -p menu.mustache -p footer.mustache view-nl.json sherborne.mustache >public/sherborne.html
-mustache -p menu.mustache -p footer.mustache view-nl.json peutertijd.mustache >public/peutertijd.html
+mustache -p menu.mustache -p footer.mustache view-nl.json aanbod.mustache >public/aanbod.html
+#mustache -p menu.mustache -p footer.mustache view-nl.json sherborne.mustache >public/sherborne.html
+#mustache -p menu.mustache -p footer.mustache view-nl.json peutertijd.mustache >public/peutertijd.html
+mustache -p menu.mustache -p footer.mustache view-nl.json media.mustache >public/media.html
+mustache -p menu.mustache -p footer.mustache view-nl.json nieuws.mustache >public/nieuws.html
 mustache -p menu.mustache -p footer.mustache view-nl.json contact.mustache >public/contact.html
+mustache -p menu.mustache -p footer.mustache view-nl.json nieuwsbrief.mustache >public/nieuwsbrief.html
